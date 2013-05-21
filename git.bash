9be24f62c6f5e16b00a160cb26f26356e1211019
Wonzback <silouane@gmail.com>
2013-05-21 12:05:54 +0200

    Git add alias + add stash list lentgh
    rename gitfile  + add git gd alias
    add git stash list length in prompt

--- git.bash
+++ git.bash
@@ -1,3 +1,4 @@
+# GIT ALIAS
 alias gcl='git clone'
 alias ga='git add'
 alias gall='git add .'
@@ -12,6 +13,7 @@ alias gpp='git pull && git push'
 alias gup='git fetch && git rebase'
 alias gp='git push'
 alias gpo='git push origin'
+alias gd='git diff'
 alias gdv='git diff -w "$@" | vim -R -'
 alias gc='git commit -v'
 alias gca='git commit -v -a'
@@ -29,3 +31,38 @@ alias gg="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %
 alias ggs="gg --stat"
 alias gsl="git shortlog -sn"
 alias gw="git whatchanged"
+
+
+#GIT PROMPT
+
+# git branch dans le prompt
+function parse_git_branch {
+  git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
+}
+
+function parse_git_status {
+  noupdated=`git status --porcelain 2> /dev/null | grep -E "^ (M|D)" | wc -l`
+  nocommitted=`git status --porcelain 2> /dev/null | grep -E "^(M|A|D|R|C)" | wc -l`
+  nostash=`git stash list 2> /dev/null | wc -l`
+
+  if [[ $noupdated -gt 0 ]]; then echo -n "*"; fi
+  if [[ $nocommitted -gt 0 ]]; then echo -n "+"; fi
+  if [[ $nostash -gt 0 ]]; then echo -n " s($nostash)";fi
+}
+
+RED="\[\033[01;31m\]"
+YELLOW="\[\033[01;33m\]"
+GREEN="\[\033[01;32m\]"
+BLUE="\[\033[01;34m\]"
+NC="\[\033[0m\]"
+
+case $TERM in
+    xterm*)
+        TITLEBAR='\[\e]0;\u@\h: \w\a\]';
+        ;;
+    *)
+        TITLEBAR="";
+        ;;
+esac
+
+PS1="${TITLEBAR}$RED\$(date +%H:%M) $GREEN\u@\h $BLUE\w$YELLOW\$(parse_git_branch)\$(parse_git_status) $BLUE\$ $NC"
